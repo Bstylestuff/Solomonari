40466f7b9aa14fbaec2a97381dcb39f70af1de91
bstylestuff <box2_2001@yahoo.com>
2022-10-01 01:17:18 +0300

    updated clean

--- Mountain/Mountain.gd
+++ Mountain/Mountain.gd
@@ -5,4 +5,4 @@ const cloud_template = preload("res://Cloud/Cloud.tscn")
 func spawn():
 	var new_cloud = cloud_template.instance()
 	new_cloud.global_position=self.global_position
-	get_parent().add_child(new_cloud)
+	get_parent().get_node("Clouds").add_child(new_cloud)
