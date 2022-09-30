bb42628e801f09c3fe7464066102fde9d23a21d0
Sorin <sorin.a@layershift.me>
2022-10-01 00:25:39 +0300

    cloud update

--- Cloud/Cloud.gd
+++ Cloud/Cloud.gd
@@ -6,7 +6,6 @@ var active = false
 var path_follow=true
 var point_to_loffow=0
 var path_to_folow=[]
-var t = 0
 export var  ttl = 500
 
 # Called when the node enters the scene tree for the first time.
@@ -20,24 +19,27 @@ func _process(delta):
 	
 	if path_to_folow.size() > 0:
 		if path_follow ==true:
-			t += delta*0.2
-			if self.global_position.distance_to(path_to_folow[point_to_loffow])>3:
-				self.global_position=self.global_position.linear_interpolate(path_to_folow[point_to_loffow],t)
+			if self.global_position.distance_to(path_to_folow[point_to_loffow])>4:
+				self.global_position=self.global_position.linear_interpolate(path_to_folow[point_to_loffow],delta*5)
 	
-	if point_to_loffow < path_to_folow.size() &&  self.global_position.distance_to(path_to_folow[point_to_loffow])<3 :
+	if point_to_loffow < path_to_folow.size() &&  self.global_position.distance_to(path_to_folow[point_to_loffow])<4 :
 		point_to_loffow +=1
 		t=0 
 	if point_to_loffow == path_to_folow.size():
 		path_to_folow.clear()
 		t=0
 		point_to_loffow =0
+	$Line2D.global_position=Vector2(0,0)
 
 
 func _input(event):
 	
 	if event is InputEventMouseMotion && active==true:
-		
-		path_to_folow.append(get_global_mouse_position())
+		if path_to_folow.size()==0:
+			path_to_folow.append(get_global_mouse_position())
+		if self.global_position.distance_to(path_to_folow[path_to_folow.size()-1])>18:
+			path_to_folow.append(get_global_mouse_position())
+			$Line2D.add_point(get_global_mouse_position())
 	if event is InputEventMouseButton:
 		if event.button_index == BUTTON_LEFT  and active==true and event.pressed==false:
 			active=false
