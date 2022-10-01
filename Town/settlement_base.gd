89115f2ab6ee2ec9b04425693a23ea19b393ae30
bstylestuff <box2_2001@yahoo.com>
2022-10-01 20:01:59 +0300

    up

--- Town/settlement_base.gd
+++ Town/settlement_base.gd
@@ -15,22 +15,34 @@ var hp: int = 300
 var crops:int = 500
 var population:int = 10
 var min_crops:int = 100
+var turn_duration:int = 5
+var curr_turn:float=0
 
 func _init():
 	money_per_tick = 5
 	happiness_level = 10
 	required_food = 2
 
-func _process(delta):
+func _physics_process(delta):
+	curr_turn+=delta
+	if(curr_turn>turn_duration):
+		_pass_turn()
+		curr_turn-=turn_duration
+		
+
+func _pass_turn():
 	_update_status()
 	_eval_happiness()
+	if(can_pay):
+		if(stored_food>required_food*population*2):
+			stored_food-=required_food*population
+			pay()
 
 func _update_status():
 	_eval_food()
 	if is_sieged == true:
 		happiness_level = happiness_level - 1
 		hp-=1
-
 	if hp==0:
 		_die()
 
@@ -42,11 +54,16 @@ func _eval_food():
 		happiness_level -= 1
 
 func _consume_food():
-	stored_food+=crops
+	
 	stored_food-=required_food*population
+	if(stored_food<0):
+		_pop_death()
+	stored_food+=crops
 	crops-=population
 	if(crops<min_crops):
 		_need()
+	if(crops>required_food*population):
+		_birth()
 
 func _eval_happiness():
 	if(happiness_level<tolerance):
@@ -72,3 +89,17 @@ func _pay_up():
 
 func _die():
 	pass
+
+func rain(amount):
+	crops+=amount
+	
+func _birth():
+	if(GameState.randomizer.randi_range(0,100)>40):
+		crops-=30
+		population+=1
+
+func _pop_death():
+	population-=1
+	happiness_level-=3
+	if(population==0):
+		_die()
--- Utils/GameState.gd
+++ Utils/GameState.gd
@@ -4,6 +4,11 @@ var score: int =0
 var spawn_rate:int = 0
 var max_enemies:int = 0
 var enemy_spawn_rate:int = 0
+var randomizer
 
+func _on_ready():
+	randomizer=RandomNumberGenerator.new()
+	randomizer.randomize()
+	
 func add_score(population):
 	score+=population
