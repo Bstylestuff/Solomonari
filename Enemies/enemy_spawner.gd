0047d503006fa1ae952dbee1c0d9945611df7b16
Sorin <sorin.a@layershift.me>
2022-10-01 16:43:21 +0300

    spawner update

--- Enemies/enemy_spawner.gd
+++ Enemies/enemy_spawner.gd
@@ -5,28 +5,22 @@ var towns=[]
 
 var rng = RandomNumberGenerator.new()
 
-
-
 func _ready():
-	var towns = get_tree().get_nodes_in_group("towns")
+	towns = get_tree().get_nodes_in_group("towns")
 	rng.randomize()
-	print(towns[3])
-
-func _process(delta):
 	spawn()
-	pass
 
 func spawn():
 	var new_enemy = enemt_template.instance()
-	new_enemy.set_target(_get_target())
+	var target = get_target()
+	print(target)
+	new_enemy.set_target(target)
 	new_enemy.global_position=self.global_position
-	get_parent().add_child(new_enemy)
-	
+	get_parent().get_node("enemy").add_child(new_enemy)
 
-func _get_target():
+func get_target():
 	var random_target = rng.randi_range(0,towns.size()-1)
-	print_debug (random_target)
-	return Vector2 (200,200)
+	return towns[random_target].global_position
 
 func i_died():
 	spawn()
