1abbec172415210b55de7e301943743cd43a09bc
bstylestuff <box2_2001@yahoo.com>
2022-10-01 18:38:11 +0300

    Town can cry

--- Town/settlement_base.gd
+++ Town/settlement_base.gd
@@ -8,11 +8,11 @@ var stored_food: int = 0
 var tolerance: int = 0
 var is_threatened: bool = false
 
-# Food grows when the crops are watered. If stored_food >= required_food happiness increases 
 func _init():
 	money_per_tick = 5
 	happiness_level = 10
 	required_food = 20
+
 func _process(delta):
 	if stored_food >= required_food:
 		happiness_level = happiness_level + 1
@@ -22,12 +22,14 @@ func _process(delta):
 		happiness_level = happiness_level - 1
 	if tolerance >= happiness_level:
 		return money_per_tick
-		
+
 func _threaten_city():
 	is_threatened = true
 
 func pay():
 	$AnimationPlayer.play("TownPay")
+func need():
+	$AnimationPlayer.play("WantRain")
+
 func _pay_up():
 	GameState.add_score()
-	
